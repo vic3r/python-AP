1f0713d0ac53309bb4ba8427ac6418a6098f146b
Victor Garcia <victor.g64r@gmail.com>
2020-09-09 11:35:20 -0500

    Integrate e2e tests Integrate integration tests Integrate unit tests Modify conftest to up pg and service

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -3,18 +3,21 @@ from domain import model
 from adapters import repository
 from service_layer import services
 
+
 class FakeRepository(repository.AbstractRepository):
+
     def __init__(self, batches):
         self._batches = set(batches)
-    
+
     def add(self, batch):
         self._batches.add(batch)
-    
+
     def get(self, reference):
         return next(b for b in self._batches if b.reference == reference)
-    
+
     def list(self):
         return list(self._batches)
+
 
 class FakeSession():
     committed = False
@@ -22,27 +25,29 @@ class FakeSession():
     def commit(self):
         self.committed = True
 
-def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
-    
-    result = services.allocate(line, repo, FakeSession())
-    assert result == "b1"
+def test_add_batch():
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, repo, session)
+    assert repo.get("b1") is not None
+    assert session.committed
 
-def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTINGSKU", 10)
-    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
-    repo = FakeRepository([batch])
+def test_allocate_returns_allocation():
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, repo, session)
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, session)
+    assert result == "batch1"
 
-    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTINGSKU"):
-        services.allocate(line, repo, FakeSession())
+def test_allocate_errors_for_invalid_sku():
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("b1", "AREALSKU", 100, None, repo, session)
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
 
 def test_commits():
-    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
-    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
-    repo = FakeRepository([batch])
+    repo, session = FakeRepository([]), FakeSession()
     session = FakeSession()
-
-    services.allocate(line, repo, session)
+    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, repo, session)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
     assert session.committed is True
+
