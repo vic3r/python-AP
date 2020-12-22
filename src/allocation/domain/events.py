3ccd0adda99bc5f08852c679736fe77cca1a67fa
Victor Garcia <victor.g64r@gmail.com>
2020-12-22 14:35:10 -0600

    Move events definition to separate file Handle events in messagebus

--- src/allocation/domain/events.py
+++ src/allocation/domain/events.py
@@ -1,8 +1,23 @@
 from dataclasses import dataclass
-
+from typing import Optional
+from datetime import date
 class Event:
   pass
 
 @dataclass
 class OutOfStock(Event):
   sku: str
+
+@dataclass
+class BatchCreated(Event):
+  ref: str
+  sku: str
+  qty: int
+  eta: Optional[date]
+
+@dataclass
+class AllocationRequired(Event):
+  orderid: str
+  sku: str
+  qty: int
+
--- src/allocation/service_layer/messagebus.py
+++ src/allocation/service_layer/messagebus.py
@@ -1,17 +1,15 @@
 from allocation.domain import events
-from allocation.adapters import email
 from allocation.domain import events
+from . import handlers, unit_of_work
 
-def handle(event: events.Event):
-    for handler in HANDLERS[type(event)]:
-        handler(event)
-
-def send_out_of_stock_notification(event: events.OutOfStock):
-    email.send_email(
-        'stock@made.com',
-        f'Out of stock for {event.sku}',
-    )
+def handle(event: events.Event, uow: unit_of_work.AbstractUnitOfWork):
+    queue = [event]
+    while queue:
+        event = queue.pop(0)
+        for handler in HANDLERS[type(event)]:
+            handler(event, uow=uow)
+            queue.extend(uow.collect_new_events())
 
 HANDLERS = {
-    events.OutOfStock: [send_out_of_stock_notification],
+    events.OutOfStock: [handlers.send_out_of_stock_notification],
 }
