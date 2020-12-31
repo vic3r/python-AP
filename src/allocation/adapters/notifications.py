f3122092e5be92e3ff6e94ea74812c1f94425002
Victor Garcia <victor.g64r@gmail.com>
2020-12-31 16:26:57 -0600

    Add notifications adapter

---
+++ src/allocation/adapters/notifications.py
@@ -0,0 +1,22 @@
+import abc
+import smtplib
+
+class AbstractNotifications(abc.ABC):
+
+  @abc.abstractmethod
+  def send(self, destination, message):
+    raise NotImplementedError
+
+class EmailNotifications(AbstractNotifications):
+
+  def __init__(self, smtp_host=DEFAULT_HOST, port=DEFAULT_PORT):
+    self.server = smtplib.SMTP(smtp_host, port=port)
+    self.server.noop()
+
+  def send(self, destination, message):
+    msg = f'Subject: allocation service notification\n{message}'
+    self.server.sendmail(
+      from_addr='allocations@example.com',
+      to_addrs=[destination],
+      msg=msg
+    )
