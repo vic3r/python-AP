ccc2fcce6cab465f5e8bd41c6047a8b62d4364d0
Victor Garcia <victor.g64r@gmail.com>
2021-01-01 18:37:26 -0600

    Import libraries in bootstrap

--- src/allocation/bootstrap.py
+++ src/allocation/bootstrap.py
@@ -1,15 +1,25 @@
-from allocation.service_layer import unit_of_work, messagebus
+import inspect
+from typing import Callable
+from allocation.adapters import orm, redis_eventpublisher
+from allocation.adapters.notifications import (
+  AbstractNotifications, EmailNotifications
+)
+from allocation.service_layer import handlers, messagebus, unit_of_work
+
 
 def bootstrap(
   start_orm: bool = True,
   uow: unit_of_work.AbstractUnitOfWork = unit_of_work.SqlAlchemyUnitOfWork(),
-  notifications: AbstractNotifications = EmailNotifications(),
+  notifications: AbstractNotifications = None,
   publish: Callable = redis_eventpublisher.publish,
-) -> messagesbus.MessageBus:
-  
+) -> messagebus.MessageBus:
+
+  if notifications is None:
+    notifications = EmailNotifications()
+
   if start_orm:
     orm.start_mappers()
-  
+
   dependencies = {'uow': uow, 'notifications': notifications, 'publish': publish}
   injected_event_handlers = {
     event_type: [
@@ -19,11 +29,8 @@ def bootstrap(
     for event_type, event_handlers in handlers.EVENT_HANDLERS.items()
   }
   injected_command_handlers = {
-    event_type: [
-      inject_dependencies(handler, dependencies)
-      for handler in command_handlers
-    ]
-    for command_type, command_handlers in handlers.COMMAND_HANDLERS.items()
+    command_type: inject_dependencies(handler, dependencies)
+    for command_type, handler in handlers.COMMAND_HANDLERS.items()
   }
 
   return messagebus.MessageBus(
@@ -32,6 +39,7 @@ def bootstrap(
     command_handlers=injected_command_handlers,
   )
 
+
 def inject_dependencies(handler, dependencies):
   params = inspect.signature(handler).parameters
   deps = {
@@ -39,5 +47,4 @@ def inject_dependencies(handler, dependencies):
     for name, dependency in dependencies.items()
     if name in params
   }
-
   return lambda message: handler(message, **deps)
