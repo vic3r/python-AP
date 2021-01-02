4a81ade2ab4213ef909bdb8bd1bd1b32b454fea2
Victor Garcia <victor.g64r@gmail.com>
2021-01-01 19:04:29 -0600

    Complete 2e2 tests

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,76 +1,38 @@
-import uuid
 import pytest
-import requests
-
-from allocation import config
-from ..random_refs import random_sku, random_batchref, random_orderid
-
-def post_to_add_batch(ref, sku, qty, eta):
-    url = config.get_api_url()
-    r = requests.post(
-        f'{url}/add_batch',
-        json={'ref': ref, 'sku': sku, 'qty': qty, 'eta': eta}
-    )
-    assert r.status_code == 201
-
-
-@pytest.mark.usefixtures('postgres_db')
-@pytest.mark.usefixtures('restart_api')
-def test_unhappy_path_returns_400_and_error_message():
-    unknown_sku, orderid = random_sku(), random_orderid()
-    data = {'orderid': orderid, 'sku': unknown_sku, 'qty': 20}
-    url = config.get_api_url()
-    r = requests.post(f'{url}/allocate', json=data)
-    assert r.status_code == 400
-    assert r.json()['message'] == f'Invalid sku {unknown_sku}'
-
-@pytest.mark.usefixtures('postgres_db')
-@pytest.mark.usefixtures('restart_api')
-def test_happy_path_returns_201_and_allocated_batch():
-    sku, othersku = random_sku(), random_sku('other')
-    earlybatch = random_batchref(1)
-    laterbatch = random_batchref(2)
-    otherbatch = random_batchref(3)
-    post_to_add_batch(laterbatch, sku, 100, '2011-01-02')
-    post_to_add_batch(earlybatch, sku, 100, '2011-01-01')
-    post_to_add_batch(otherbatch, sku, 100, None)
-    data = {'orderid': random_orderid(), 'sku': sku, 'qty': 3}
-    url = config.get_api_url()
-    r = requests.post(f'{url}/allocate', json=data)
-    assert r.status_code == 201
-    assert r.json()['batchref'] == earlybatch
+from ..random_refs import random_batchref, random_orderid, random_sku
+from . import api_client
 
 @pytest.mark.usefixtures('postgres_db')
 @pytest.mark.usefixtures('restart_api')
 def test_happy_path_returns_202_and_batch_is_allocated():
-    orderid = random_orderid()
-    sku, othersku = random_sku, random_sku('other')
-    earlybatch = random_batchref(1)
-    laterbatch = random_batchref(2)
-    earlybatch = random_batchref(3)
-    api_client.post_to_add_batch(laterbatch, sku, 100, '2011-01-02')
-    api_client.post_to_add_batch(earlybatch, sku, 100, '2011-01-01')
-    api_client.post_to_add_batch(otherbatch, sku, 100, None)
+	orderid = random_orderid()
+	sku, othersku = random_sku(), random_sku('other')
+	earlybatch = random_batchref(1)
+	laterbatch = random_batchref(2)
+	otherbatch = random_batchref(3)
+	api_client.post_to_add_batch(laterbatch, sku, 100, '2011-01-02')
+	api_client.post_to_add_batch(earlybatch, sku, 100, '2011-01-01')
+	api_client.post_to_add_batch(otherbatch, othersku, 100, None)
 
-    r = api_client.post_to_allocate(orderid, sku, qty=3)
-    assert r.status_code == 202
+	r = api_client.post_to_allocate(orderid, sku, qty=3)
+	assert r.status_code == 202
 
-    r = api_client.get_allocation(orderid)
-    assert r.ok
-    assert r.json() == [
-        {'sku': sku, 'batchref': earlybatch},
-    ]
+	r = api_client.get_allocation(orderid)
+	assert r.ok
+	assert r.json() == [
+		{'sku': sku, 'batchref': earlybatch},
+	]
 
 
 @pytest.mark.usefixtures('postgres_db')
 @pytest.mark.usefixtures('restart_api')
 def test_unhappy_path_returns_400_and_error_message():
-    unknown_sku, orderid = random_sku(), random_orderid()
-    r = api_client.post_to_allocate(
-        orderid, unknown_sku, qty=20, except_success=False
-    )
-    assert r.status_code == 404
-    assert r.json()['message'] == f'Invalid sku {unknown_sku}'
+	unknown_sku, orderid = random_sku(), random_orderid()
+	r = api_client.post_to_allocate(
+		orderid, unknown_sku, qty=20, expect_success=False,
+	)
+	assert r.status_code == 400
+	assert r.json()['message'] == f'Invalid sku {unknown_sku}'
 
-    r = api_client.get_allocate(orderid)
-    assert r.status_code == 404
+	r = api_client.get_allocation(orderid)
+	assert r.status_code == 404
